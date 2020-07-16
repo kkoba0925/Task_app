0b64fb55fc0219e51513903752487e8a861b2363
Kentarou Koba <k.koba0925@gmail.com>
2020-07-16 23:00:54 +0900

    view create

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
 <template>
 
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Studying JavaScript</td>
+      <td>
+        <div class="btn btn-danger">Done!</div>
+      </td>
+    </tr>
+    <tr>
+      <td>Studying PHP</td>
+      <td>
+        <div class="btn btn-danger">Done!</div>
+      </td>
+    </tr>
+    <tr>
+      <td>Studying Ruby</td>
+      <td>
+        <div class="btn btn-danger">Done!</div>
+      </td>
+    </tr>
+    <tr>
+      <td><input class="form-control"></td>
+      <td>
+        <div class="btn btn-default">Create Task</div>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
 </template>
 
 <script>
